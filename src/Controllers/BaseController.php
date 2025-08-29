4a1b8f26eeae5377a625e1ec999bd5db57f8cd9d
Laurie1985 <berthon.laurie@neuf.fr>
2025-08-29 12:28:44 +0200

    Modification BaseController.php: Ajout vérification existence fichier de vue

---
+++ src/Controllers/BaseController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controllers;
+
+// Classe abstraite de base pour les contrôleurs
+abstract class BaseController
+{
+    // Méthode pour renvoyer une vue
+    public function render(string $view, array $data = []): void
+    {
+        // Extraction du contenu des données dans des variables
+        extract($data);
+
+                                         // Chemin de la vue
+        $rootDir  = dirname(__DIR__, 2); // Remonte de deux niveaux depuis src/Controllers/
+        $viewFile = $rootDir . '/Views/' . $view . '.php';
+
+        // Vérification que le fichier de la vue existe
+        if (! file_exists($viewFile)) {
+            throw new \Exception("Vue non trouvée : {$viewFile}");
+        }
+
+        ob_start(); // Démarre la mise en tampon de sortie
+
+        // Inclusion de la vue
+        require_once $viewFile;
+
+        $content = ob_get_clean(); // Récupère le contenu du tampon et nettoie le tampon de sortie
+
+        // Inclusion du layout principal
+        $layoutFile = $rootDir . 'Views/layout.php';
+        if (file_exists($layoutFile)) {
+            require_once $layoutFile;
+        } else {
+            echo $content; // Si le layout n'existe pas, affiche juste le contenu
+        }
+    }
+}
