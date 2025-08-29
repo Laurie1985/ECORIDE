24f9b53677215c40f30ea66adda90099c405336c
Laurie1985 <berthon.laurie@neuf.fr>
2025-08-29 11:32:06 +0200

    Ajout index.php, point d'entrée app

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
-<?php phpinfo(); ?>
+<?php
+
+use App\Config\Config;
+use App\lib\Router;
+
+session_start();
+
+// Chargement des variables d'environnement
+Config::load();
+
+// Remonte d'un niveau depuis public/
+$rootDir = dirname(__DIR__);
+
+// Chargement de l'autoloader Composer
+require_once $rootDir . '/vendor/autoload.php';
+
+// Configuration (fichier, pas une classe)
+require_once $rootDir . '/config/database.php';
+
+// Initialisation du routeur
+$router = new Router();
+
+// Dispatch de la requête
+$router->dispatch();
