2441c7bcfda7eab06f377a9e5d5c3dc5b6b482cf
Laurie1985 <berthon.laurie@neuf.fr>
2025-08-29 11:34:33 +0200

    Ajout fichier Config.php pour variables env

---
+++ src/Config/Config.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Config;
+
+use Dotenv\Dotenv;
+
+class Config
+{
+    /**Classe pour charger le fichier .env, le lire, séparer et nettoyer les données
+    * et fournir une méthode statique pour accéder aux variables d'environnement
+    */
+
+    /**
+     * @param string $path le chemin vers le dossier contenant le fichier .env
+     */
+
+    public static function load($path = null): void
+    {
+        // Si aucun chemin fourni, utilise le dossier racine du projet
+        if ($path === null) {
+            // Depuis src/Config/, on remonte de 2 niveaux pour atteindre la racine
+            $path = dirname(__DIR__, 2) . '/';
+        }
+        //On vérifie si le fichier .env existe avant de tenter de le charger
+        if (file_exists($path . '.env')) {
+            $dotenv = Dotenv::createImmutable($path);
+            $dotenv->load();
+        }
+    }
+    /**
+     * @param string $key le nom de la variable d'environnement
+     * @param mixed $default la valeur par défaut si la variable n'est pas définie
+     * @return mixed la valeur de la variable d'environnement ou la valeur par défaut
+     */
+
+    public static function get(string $key, $default = null)
+    {
+        return $_ENV[$key] ?? $default;
+    }
+}
