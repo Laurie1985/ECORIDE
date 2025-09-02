c541ae18c5c9c9d6708053181aa51c45e33fff54
Laurie1985 <berthon.laurie@neuf.fr>
2025-09-02 11:34:15 +0200

    Modifications configuration

--- public/index.php
+++ public/index.php
@@ -1,12 +1,5 @@
 <?php
-
-use App\Config\Config;
-use App\lib\Router;
-
 session_start();
-
-// Chargement des variables d'environnement
-Config::load();
 
 // Remonte d'un niveau depuis public/
 $rootDir = dirname(__DIR__);
@@ -14,8 +7,11 @@ $rootDir = dirname(__DIR__);
 // Chargement de l'autoloader Composer
 require_once $rootDir . '/vendor/autoload.php';
 
-// Configuration (fichier, pas une classe)
-require_once $rootDir . '/config/database.php';
+use App\Config\Config;
+use App\lib\Router;
+
+// Chargement des variables d'environnement
+Config::load();
 
 // Initialisation du routeur
 $router = new Router();
